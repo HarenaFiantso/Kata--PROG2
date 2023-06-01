5a1a0516437dd9adced8229cf17444b8116dbd0a
Fiantso Harena <124450883+fiantsoharena2@users.noreply.github.com>
2023-06-01 12:00:37 +0300

    Fit: Rounded output to 3 decimals

--- Kata3.java
+++ Kata3.java
@@ -1,16 +1,23 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Kata3 {
     public static List<Double> fetchScoreDetails(double[] scores) {
         Arrays.sort(scores);
+
         List<List<Double>> combinations = generatePossibleCombinations(scores, 9);
 
         double minScore = calculateMinScore(combinations);
         double averageScore = calculateAverageScore(combinations);
         double medianScore = calculateMedianScore(scores);
         double maxScore = calculateMaxScore(combinations);
+
+        minScore = roundToThreeDecimalPlaces(minScore);
+        averageScore = roundToThreeDecimalPlaces(averageScore);
+        medianScore = roundToThreeDecimalPlaces(medianScore);
+        maxScore = roundToThreeDecimalPlaces(maxScore);
 
         return Arrays.asList(minScore, averageScore, medianScore, maxScore);
     }
@@ -85,17 +92,17 @@ public class Kata3 {
         return sum / numbers.size();
     }
 
+    private static double roundToThreeDecimalPlaces(double value) {
+        return Math.round(value * 1000.0) / 1000.0;
+    }
+
     public static void main(String[] args) {
         double[] input1 = new double[]{6.7, 4.2, 4.3, 7.0, 0.0, 2.8, 3.9, 1.3, 6.9, 6.0, 7.3, 3.2};
         double[] input2 = new double[]{7.1, 9.6, 0.9, 3.9, 7.4, 3.3, 7.5, 2.6, 2.9, 8.4, 6.4, 0.3};
 
-        // SHOULD RETURN [3.600, 4.667, 4.250, 5.500]
-        // min 3.600 average 4.467 median 4.250 max 5.500
         List<Double> scoreDetails1 = fetchScoreDetails(input1);
         System.out.println(scoreDetails1);
 
-        // SHOULD RETURN [3.867, 5.025, 5.150, 6.278]
-        // min 3.867 average 5.025 median 5.150 max 6.278
         List<Double> scoreDetails2 = fetchScoreDetails(input2);
         System.out.println(scoreDetails2);
     }
